ca8790d7e91af8f04296ae780703c38f02c5b2e3
seraph.moe <156139427+seraphmoe@users.noreply.github.com>
2026-04-15 03:34:12 +0100

    remove json data

--- bot_config.py
+++ bot_config.py
@@ -1,26 +1,12 @@
 # bot_config
 
+from pathlib import Path
+
+home_dir = Path.home()
+
 APP_NAME = 'BlueSky bot service'
 CMD_ESC = 'Ctrl+C'
 
 QUERY = 'puppygirl art'
 
-DATA_FILE = "bot_data.json"
-
-LEXICON = [
-    'so puppy',
-    'puppy',
-    'wehhhh puppy',
-    'puppy is wanted in at least 37 countries and counting !',
-    'haihaiiiii :3',
-    'puppy doesnt know many words wehhh',
-    'two or more puppygirls dating is basically incest',
-    'puppy runs on some dusty ass repurposed motorola phone that @seraph.moe found inside its gasmask bag',
-    'wehh,, people assume that puppy is weak,, puppy is an angel, and angels purify meanies',
-    'pluhh pluhhhh,,, *pees on ur bed*',
-    'puppy is a little bit broken but its oki ! who isnt a lil broken ?',
-    'some say puppy is just a part of @seraph.moe soul',
-    'report all unpuppylike behaviour to @seraph.moe',
-    'so smol,, so fur,, so soft,,,',
-    'all girls are magical,,'
-    ]
+DATA_FILE = f"{home_dir}/bot_data.json"
